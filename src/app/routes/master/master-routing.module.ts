3d5c86f07882c4e178f8ff0787808c6ad7f53489
dharmendra <dharmendra.bhadoria@apaarinfosystems.com>
2022-09-14 13:03:42 +0530

    add by dharmendra

--- src/app/routes/master/master-routing.module.ts
+++ src/app/routes/master/master-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
 import { ProductComponent } from './product/product.component';
-
+import { LabourContractorsComponent } from './labour-contractors/labour-contractors.component';
 const routes: Routes = [
   { path: 'product', component: ProductComponent },
   { path: 'items', component: ItemsComponent },
+  { path: 'Labour Contractors', component: LabourContractorsComponent },
 ];
 
 @NgModule({
