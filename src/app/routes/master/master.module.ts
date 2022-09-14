790eee8c85e2cc80cabaca9f521cdbaf3c14da73
VishalSchool <vishalmane245@gmail.com>
2022-09-14 15:57:24 +0530

    conflict changes

--- src/app/routes/master/master.module.ts
+++ src/app/routes/master/master.module.ts
@@ -7,8 +7,8 @@ import { ItemsComponent } from './items/items.component';
 import { LabourContractorsComponent } from './labour-contractors/labour-contractors.component';
 
 import { PelletsComponent } from './pellets/pellets.component';
-<<<<<<< HEAD
-=======
+
+
 import { ReportPrintInfoComponent } from './report-print-info/report-print-info.component';
 import { CheckListMasterComponent } from './check-list-master/check-list-master.component';
 import { CheckListDetailComponent } from './check-list-detail/check-list-detail.component';
@@ -16,19 +16,19 @@ import { DockMasterComponent } from './dock-master/dock-master.component';
 import { FinancialYearComponent } from './financial-year/financial-year.component';
 import { RemarksComponent } from './remarks/remarks.component';
 
->>>>>>> 841800c2acdf32dd418c17ff1aebef97fa6876ab
+
 
 @NgModule({
   declarations: [
     ProductComponent,
     ItemsComponent,
-<<<<<<< HEAD
+
 
     LabourContractorsComponent,
 
-    PelletsComponent
+    PelletsComponent,
 
-=======
+
     LabourContractorsComponent,
     PelletsComponent,
     ReportPrintInfoComponent,
@@ -37,7 +37,7 @@ import { RemarksComponent } from './remarks/remarks.component';
     DockMasterComponent,
     FinancialYearComponent,
     RemarksComponent
->>>>>>> 841800c2acdf32dd418c17ff1aebef97fa6876ab
+
   ],
   imports: [
     SharedModule,
