920078cef20ca9060a2306e57fceb19701c75a2b
ravi <ravi.chaudhari@apaarinfosystems.com>
2022-09-14 17:37:27 +0530

    add get pending do

--- src/app/routes/master/master.module.ts
+++ src/app/routes/master/master.module.ts
@@ -3,21 +3,18 @@ import { MasterRoutingModule } from './master-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { ProductComponent } from './product/product.component';
 import { ItemsComponent } from './items/items.component';
-<<<<<<< HEAD
+
 import { LabourContractorsComponent } from './labour-contractors/labour-contractors.component';
-=======
+
 import { PelletsComponent } from './pellets/pellets.component';
->>>>>>> d7fe0f1c464cf4445749edc1c040015e117afb17
+
 
 @NgModule({
   declarations: [
     ProductComponent,
     ItemsComponent,
-<<<<<<< HEAD
-    LabourContractorsComponent
-=======
+    LabourContractorsComponent,
     PelletsComponent
->>>>>>> d7fe0f1c464cf4445749edc1c040015e117afb17
   ],
   imports: [
     SharedModule,
--- src/app/routes/process/outward/outward.component.ts
+++ src/app/routes/process/outward/outward.component.ts
@@ -14,22 +14,26 @@ export class OutwardComponent implements OnInit {
   customePartyList:any;
   remarkList:any;
   form!: FormGroup;
+  formParty!: FormGroup;
   tab=0;
   todayDate : any ;
   dockList:any;
   unloadingList:any;
   labourContractorList:any;
+  
   config = {
     backdrop: false,
     ignoreBackdropClick: true
   };
-  
+  SaveCustomerParty: any = {};  
+  GetPendingDO: any = {};  
+
   private currentUser:User;
-  constructor(private api:ApiService,private fb: FormBuilder,private modalService: NgbModal) {
+  constructor(private api:ApiService,private fb: FormBuilder,private fbParty: FormBuilder,private modalService: NgbModal) {
     this.currentUser=this.api.getCurrentUser();
     const dateSendingToServer = new DatePipe('en-US').transform(Date(), 'yyyy-MM-dd')
     this.todayDate=dateSendingToServer;
-
+    this.currentUser=this.api.getCurrentUser();
     this.form = this.fb.group({
       customer_id: [null, Validators.required],
       Outward_Date: [null, Validators.required],
@@ -43,20 +47,24 @@ export class OutwardComponent implements OnInit {
       cbLodingBy_Name: [null, Validators.required],
       cbLabourContractor: [null, Validators.required],
     });
+    this.formParty =this.fbParty.group({
+      txtPartyName: ['', Validators.required],
+      txtShippingAddress1: ['', Validators.required],
+      txtShippingAddress2: ['', Validators.required],
+      txtPinCode: ['', Validators.required],
+    });
    }
 
   ngOnInit(): void {
     this.BindDropdown();
+    this.BindCustomerParty();
   }
   async BindDropdown(){
     this.api.get('/Customer').subscribe(
      data=>{this.customerList=data},
      error=>{ console.error(error);}
    );
-   this.api.get('/CustomerParty').subscribe(
-     data=>{this.customePartyList=data;},
-     error=>{ console.error(error);}
-   );
+   
    this.api.get('/Remarks').subscribe(
      data=>{this.remarkList=data.filter((p:any)=>p.ProcessID==3);},
      error=>{ console.error(error);}
@@ -74,26 +82,78 @@ export class OutwardComponent implements OnInit {
     error=>{ console.error(error);}
   );
  }
+
+ BindCustomerParty(){
+  this.api.get('/CustomerParty').subscribe(
+    data=>{this.customePartyList=data;},
+    error=>{ console.error(error);}
+  );
+ }
   tabDeliveryOrderchange(event:any){
 
   }
-  OnSearchClick(){
+  OnSearchPendingDO(){
+    if(this.form.value.customer_id==null || this.form.value.customer_id=="")
+    {
+      alert("Please .... Select Customer Name");
+      document?.getElementById("customer_id")?.focus();
+    }else{
+      this.GetPendingDO={
+        CustomerID :this.form.value.customer_id,
+        WarehouseId :this.currentUser.warehouseId,
+        CompanyID :this.currentUser.companyId
+      }
+      this.api.post('/Outward/GetDeliveryOrderByID',this.GetPendingDO).subscribe(
+        data=>{data;
+        console.log(data);},
+        error=>{ console.error(error);}
+      );
+    }
 
+    
   }
   openPartyWindow(AddPartyContent:any) {
-    console.log(this.form.value.customer_id);
-    // if(this.form.value.customer_id==null || this.form.value.customer_id=="")
-    // {
-    //   alert("Please .... Select Customer Name");
-    //   document?.getElementById("customer_id")?.focus();
-    // }else{
-      
-    // }  
-    this.modalService.open(AddPartyContent);
+    //console.log(this.form.value.customer_id);
+    if(this.form.value.customer_id==null || this.form.value.customer_id=="")
+    {
+      alert("Please .... Select Customer Name");
+      document?.getElementById("customer_id")?.focus();
+    }else{
+      this.modalService.open(AddPartyContent);
+    }  
   }
 
   onCloseAddParty(AddPartyContent:any){    
     this.modalService.dismissAll(AddPartyContent);
   }
+  onAddParty(){
+    if(this.formParty.value.txtPartyName==null || this.formParty.value.txtPartyName==""){
+      alert("Please .... Enter Party Name");
+      document?.getElementById("txtPartyName")?.focus();
+      }
+      else
+      {
+      this.SaveCustomerParty={
+        CustomerPartyID :0,
+        CustomerID :this.form.value.customer_id ,
+        PartyName : this.formParty.value.txtPartyName ,
+        ShippingAddress :this.formParty.value.txtShippingAddress1 ,
+        ShippingAddress1 :this.formParty.value.txtShippingAddress2 ,
+        PinCode :this.formParty.value.txtPinCode ,
+        CreatedBy :this.currentUser.userId
+      };
+        this.api.post('/CustomerParty/CustomerPartysave',this.SaveCustomerParty).subscribe(
+          data=>{data;
+          alert(data);
+          this.formParty.reset();
+          this.BindCustomerParty();
+        },
+        error=>{ console.error(error);}
+      );
+    }
+  }
+
+
 
 }
+
