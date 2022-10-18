6776bdfb7ff5679c269262ba0092dbaa8dcfaea3
VishalSchool <vishalmane245@gmail.com>
2022-10-18 10:54:30 +0530

    changes

--- src/app/routes/master/brands/brands.component.ts
+++ src/app/routes/master/brands/brands.component.ts
@@ -12,21 +12,15 @@ import { ColDef, GridApi } from 'ag-grid-community';
   styleUrls: ['./brands.component.scss']
 })
 export class BrandsComponent implements OnInit {
-  form!: FormGroup; submitted = false; Reseted = false;
+
+  form!: FormGroup; submitted = false;
   UserID: any = 0; BrandsList: any;
-  HideSaveButton = true;
   SaveData: any = {};
-<<<<<<< HEAD
   tab = 0;
   BrandID:number=0;
   private currentUser: User;
   HideSaveButton = true;
   frameworkComponents: any;
-=======
-  BrandID: number = 0;
-
-  private currentUser: User;
->>>>>>> 93a8eae4f7a83844c86d2015b1366122e5713676
 
   constructor(private fb: FormBuilder, private api: ApiService, public dialog: MtxDialog) {
     this.currentUser = this.api.getCurrentUser();
@@ -34,6 +28,8 @@ export class BrandsComponent implements OnInit {
       buttonRenderer: BrandsEditButtonComponent,
     }
   }
+
+
   ngOnInit(): void {
 
     this.form = this.fb.group({
@@ -42,31 +38,33 @@ export class BrandsComponent implements OnInit {
       BrandName: ["", Validators.required],
       IsActive: true,
       CreatedBy: [this.UserID]
+
     });
-    this.BindinfDataToList();
+
+    this.BindDropdown();
+
 
   }
 
-  BindinfDataToList() {
+
+  async BindDropdown() {
     this.api.get('/Brands/Brand_Select').subscribe(
-<<<<<<< HEAD
       data => { this.BrandsList = data;  },
-=======
-      data => { this.BrandsList = data; console.log("initialdata", this.BrandsList) },
->>>>>>> 93a8eae4f7a83844c86d2015b1366122e5713676
       error => { console.error(error); }
     );
   }
 
   get f() { return this.form.controls; }
+
+
   onSubmit(formData: any) {
     // console.log(formData)
     this.submitted = true;
     if (this.form.invalid) {
+
       return;
     }
     else {
-
       this.HideSaveButton = false;
       this.SaveData = {
         BrandID: this.BrandID,
@@ -90,37 +88,8 @@ export class BrandsComponent implements OnInit {
     }
   }
 
-      this.HideSaveButton = true;
-
-      {
-        this.SaveData = {
-          BrandID: this.form.value.BrandID,
-          BrandName: this.form.value.BrandName,
-          isActive: this.form.value.IsActive,
-          CreatedBy: this.currentUser.userId
-        }
-        console.log("SaveData", this.SaveData);
-        this.api.post('/Brands/Brand_Insert_Update', this.SaveData).subscribe(
-          data => {
-            this.dialog.alert(data[0], '', () => { window.location.reload(); });
-            this.onReset();
-            this.BindinfDataToList();
-          },
-          error => { console.error(error); }
-        );
-      }
-    }
-
-  }
-  onReset() {
-    this.form.reset();
-    this.BrandID = 0;
-    this.submitted = false;
-    this.HideSaveButton = true;
-
-    this.BindinfDataToList();
->>>>>>> 93a8eae4f7a83844c86d2015b1366122e5713676
-
+  tabchange(event: any) {
+    this.tab = event;
   }
   // ResetForm() {
   //   this.BrandID=0;
@@ -131,29 +100,18 @@ export class BrandsComponent implements OnInit {
   // }
 
   editProduct(record: any) {
-<<<<<<< HEAD
    this.BrandID=record.BrandID;
-=======
-    console.log("record ", record);
-
-    this.HideSaveButton = true;
->>>>>>> 93a8eae4f7a83844c86d2015b1366122e5713676
     const item: any = {
       BrandName: record.BrandName,
       CreatedBy: record.CreatedBy,
-      IsActive: record.IsActive,
+      IsActive: true
     }
     this.form.setValue(item);
     console.log(this.form.value);
-<<<<<<< HEAD
     this.tab = 0;
   }
 
   columns: ColDef[] = [
-=======
-  }
-  columns: MtxGridColumn[] = [
->>>>>>> 93a8eae4f7a83844c86d2015b1366122e5713676
     {
       headerName: 'Action', width: 100, floatingFilter: false,
       cellRenderer: "buttonRenderer",
@@ -193,5 +151,16 @@ export class BrandsComponent implements OnInit {
       sortable: true,
       minWidth: 280,
     }
+
+
+
   ]
+
+
+
+
+
+
+
+
 }
--- src/app/routes/setup/setup-routing.module.ts
+++ src/app/routes/setup/setup-routing.module.ts
@@ -8,11 +8,11 @@ import { WarehouseComponent } from './warehouse/warehouse.component';
 const routes: Routes = [
   { path:"Storagearea", component: StorageAreaComponent},
   { path:"Company", component: CompanyComponent},
-<<<<<<< HEAD
-  { path:"Warehouse", component: WarehouseComponent}
-=======
+
+  { path:"Warehouse", component: WarehouseComponent},
+
   {path:"servicemaster",component:ServiceMasterComponent}
->>>>>>> 93a8eae4f7a83844c86d2015b1366122e5713676
+
 
 
 ];
