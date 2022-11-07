8454cac44a9589760848863b458a36b2f3db24d1
vikas <vikas.sable@apaarinfosystems.com>
2022-11-07 18:43:12 +0530

    change by vikas on 7/11/22

--- src/app/routes/customers/customers.module.ts
+++ src/app/routes/customers/customers.module.ts
@@ -3,31 +3,29 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerMasterComponent } from './customer-master/customer-master.component';
-<<<<<<< HEAD
+
 import { AgGridModule } from 'ag-grid-angular';
 import { CustomermastereditButtonComponent } from './customer-master/customermasteredit-button/customermasteredit-button.component';
-@NgModule({
-  declarations: [
-    CustomerMasterComponent,
-    CustomermastereditButtonComponent
-  ],
-  imports: [
-    CommonModule,SharedModule,
-    CustomersRoutingModule,AgGridModule
-=======
 import { CustomerViewComponent } from './customer-view/customer-view.component';
-import { AgGridModule } from 'ag-grid-angular';
+
 
 @NgModule({
   declarations: [
     CustomerMasterComponent,
-    CustomerViewComponent
+    CustomermastereditButtonComponent,
+    CustomerMasterComponent,
+    CustomerViewComponent,
   ],
   imports: [
     CommonModule,SharedModule,
+    CustomersRoutingModule,AgGridModule,
+    CommonModule,SharedModule,
     CustomersRoutingModule,
     AgGridModule.withComponents([]),
->>>>>>> 58c1aeaef0d149c00a8bd087d43cc201efb0f070
+
+
+
+
   ]
 })
 export class CustomersModule { }
