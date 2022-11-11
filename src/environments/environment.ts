961fa10d90fae1b4fcf8a4117b2c61c7f065b37f
dharmendra <dharmendra.bhadoria@apaarinfosystems.com>
2022-11-11 11:36:44 +0530

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   //baseUrl: 'http://localhost:50999/api',
 baseUrl:'http://localhost:50191/api', 
 FileUploadUrl:'http://localhost:50191/GenricFileUpload.ashx',
-ServerDomianUrl:'http://www.apaar.com/FileLOcation/',
+ServerDomianFileLocationUrl:'http://www.apaar.com/FileLOcation/',
   //baseUrl:'http://localhost:50191/', 
   //baseUrl:'http://localhost:5281',  
   localUrl:'',
